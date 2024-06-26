28819028e9545c83a299f2dbef0104acb5e65fee
Bryce <brckolu192@gmial.com>
2024-06-26 17:38:55 +1000

    50 cm

--- example_code/from_zac_apelt/copied_from_the_chat.py
+++ example_code/from_zac_apelt/copied_from_the_chat.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 # You then want to relate the pixel locations (where you clicked) to the real locations (how deep and how left or right in whatever units the point is in reality) in this form:
-pts1 = np.float32([[180,132],[433,114],[587,156],[221,202]]) #pixels
-pts2 = np.float32([[0,30],[0,51],[29.5,51],[29.5,30]]) #cm
+pts1 = np.float32([[43,142],[77,106],[304,101],[354,130]]) #pixels
+pts2 = np.float32([[-14.75,50],[-14.75,71],[14.5,71],[14.5,50]]) #cm
 
 # you can then use this function to find the matrix 
 matrix = cv2.getPerspectiveTransform(pts1, pts2)
