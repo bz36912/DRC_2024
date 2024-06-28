540030af65936de41a15ce8bc6d47bb8e6e72e1c
Bolong Zhang <s4702952@student.uq.edu.au>
2024-06-28 13:32:11 +1000

    pp update

--- path_planner_2.py
+++ path_planner_2.py
@@ -5,13 +5,20 @@ def simple_diff_path_planner(blueTrans:np.ndarray, yellowTrans:np.ndarray, purpl
         # rightBlue = blueTrans[blueTrans[::, 0] > 0]
         # leftYellow = yellowTrans[yellowTrans[::, 0] < 0]
     rightBlue = blueTrans[blueTrans[::,0] > -30]
+    rightBlue = rightBlue[rightBlue[0,::] < 120]
     leftYellow = yellowTrans
     if rightBlue.size == 0 and leftYellow.size == 0: # no data
         return 0, 90
 
     if rightBlue.size > leftYellow.size: # follow the blue
-        refX = OPTIMAL_SIDE_DISTANCE
-        x, y = rightBlue[::, 0], rightBlue[::, 1]
+        blueMiddle = rightBlue[rightBlue[::,0] < 30]
+        x = rightBlue[::,0]
+        if blueMiddle.size > 5:
+            angle = blueMiddle.size
+        # refX = OPTIMAL_SIDE_DISTANCE
+        # x, y = rightBlue[::, 0], rightBlue[::, 1]
+        elif np.mean(x) > 50:
+    
     else: # follow the yellow
         refX = -OPTIMAL_SIDE_DISTANCE
         x, y = leftYellow[::, 0], leftYellow[::, 1]
