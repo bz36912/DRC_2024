3178ef8f62022b0c0ab8eda51f18df76aa217f33
Bryce <brckolu192@gmial.com>
2024-07-05 15:03:29 +1000

    planner 2 update

--- path_planner_2.py
+++ path_planner_2.py
@@ -1,14 +1,19 @@
 import numpy as np
 from car_remote_control import Uart
-OPTIMAL_SIDE_DISTANCE = 30 # cm
+
+MAX_X = 100
+MAX_Y = 120
+TOO_BIG = 50
 
 def simple_diff_path_planner(blueTrans:np.ndarray, yellowTrans:np.ndarray, purpleTrans:np.ndarray, uart:Uart):
         # rightBlue = blueTrans[blueTrans[::, 0] > 0]
         # leftYellow = yellowTrans[yellowTrans[::, 0] < 0]
     rightBlue = blueTrans[blueTrans[::,0] > -30]
-    rightBlue = rightBlue[rightBlue[::,1] < 120]
+    rightBlue = rightBlue[rightBlue[::,1] < MAX_Y]
+    rightBlue = rightBlue[rightBlue[::,1] > 0]
     leftYellow = yellowTrans[yellowTrans[::,0] > -30]
-    leftYellow = leftYellow[leftYellow[::,1] < 120]
+    leftYellow = leftYellow[leftYellow[::,1] < MAX_Y]
+    leftYellow = leftYellow[leftYellow[::,1] > 0]
     # leftYellow = yellowTrans
     if rightBlue.size == 0 and leftYellow.size == 0: # no data
         return 0, 130
@@ -17,11 +22,18 @@ def simple_diff_path_planner(blueTrans:np.ndarray, yellowTrans:np.ndarray, purpl
         blueMiddle = rightBlue[rightBlue[::,0] < 40]
         x = rightBlue[::,0]
         if blueMiddle.size > 5:
-            angle = blueMiddle.size/5
+            angle = (np.mean(blueMiddle)-40)*2
+            if np.mean(rightBlue[::,1]) <50:
+                angle = angle* -50/(np.mean(rightBlue[::,1]))
+            else:
+                angle= angle*-2
+
         # refX = OPTIMAL_SIDE_DISTANCE
-        # x, y = rightBlue[::, 0], rightBlue[::, 1]
+        # x, y = rightBlue[:qp:, 0], rightBlue[::, 1]
         elif np.mean(x) > 50:
             angle = -np.mean(x)+50
+
+            angle = angle *1
         else:
             angle = 0
     
@@ -41,16 +53,11 @@ def simple_diff_path_planner(blueTrans:np.ndarray, yellowTrans:np.ndarray, purpl
     # averageDiff = np.mean(diff)
     # angle = -averageDiffq
     # speed = 150
-    angle = min(80, max(-80, angle/5))
+    angle = min(80, max(-80, angle))
     if (abs(angle)<10):
         speed = 110
     elif (abs(angle) > 60):
         speed = -10
-        angle = 0
-        if (angle > 0):
-            uart.swing_left()
-        else: 
-            uart.swing_right()
     else: 
         speed = 130
     return angle, speed
